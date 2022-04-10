3fec05fc0e28402ef29d0c3e897c6b0d4f34f640
Roman Rihter <teledurak@gmail.com>
2022-04-10 15:50:47 +0300

    m1892: Bye-bye prebuilt LKM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,5 @@ TARGET_OTA_ASSERT_DEVICE := m1892,16thPlus
 # Kernel
 TARGET_KERNEL_CONFIG := ../m1892_defconfig
 
-# Kernel Modules
-BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DEVICE_PATH)/kernel/lkm/*.ko)
-
 # Proprietary BoardConfig
 -include vendor/meizu/m1892/BoardConfigVendor.mk
